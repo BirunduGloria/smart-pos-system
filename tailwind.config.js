d1350efece1bd47388071b6df8d166bab2f6971d
Gideon <gideonkimaiyo254@gmail.com>
2025-07-23 08:18:23 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}", // in case you're using /src
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
